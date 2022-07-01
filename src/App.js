c1f94df358d59d52dd8cc5e93de45a0bcabd8163
Shaharina Mita <S.mita90232@gmail.com>
2022-07-01 16:29:25 +0600

    style added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Footer from './Components/Footer';
 
 function App() {
     return (
         <div className='App'>
+            <hr />
             <Counter></Counter>
+            <br />
+            <hr />
+            <ExternalUsers></ExternalUsers>
+        </div>
+    );
+}
+function ExternalUsers() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, []);
+    return (
+        <div>
+            <h2>EXTERNAL USERS</h2>
+            <p> {users.length} </p>
+            {users.map(user => (
+                <User name={user.name}></User>
+            ))}
+        </div>
+    );
+}
+
+function User(props) {
+    return (
+        <div style={{ border: '2px solid white', margin: '20px' }}>
+            <h3>Name : {props.name} </h3>
+            <p>Email : {props.email} </p>
         </div>
     );
 }
